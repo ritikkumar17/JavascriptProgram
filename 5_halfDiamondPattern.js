8b48ba3a68195a34d17df6519d5008e2dd3f4c88
ritik kumar <ritikchugh17@gmail.com>
2026-05-25 15:02:56 +0530

    5 Half Diamond Pattern

---
+++ 5_halfDiamondPattern.js
@@ -0,0 +1,18 @@
+// Half Diamond patten
+
+for (let i = 1; i<=5; i++){
+    let str = ""
+    for(let j=1 ; j<=i; j++){
+        str +="*"
+    }
+    console.log(str)
+}
+    for(let k = 4; k>= 1; k--){
+        let str1 = ""
+        for(let r = 1 ; r<=k; r++ ){
+            str1 += "*"
+        }
+      console.log(str1)
+    }
+  
+
