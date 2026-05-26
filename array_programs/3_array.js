8bb8b9e05fc1d75cb22c9646663749090c11a655
ritik kumar <ritikchugh17@gmail.com>
2026-05-26 17:00:00 +0530

    reverse an aray

---
+++ array_programs/3_array.js
@@ -0,0 +1,27 @@
+// 3 Reverse an Array
+
+// 1st way
+// let arr = [1,2,3,4]
+// let rev = arr.reverse()
+// console.log(rev)
+
+
+
+// 2nd way
+let arr = [1,2,3,4]
+let rev =[]
+
+for (let num of arr){
+    rev.unshift(num)
+}
+console.log(rev)
+
+//3rd way
+let arr = [1,2,3,4]
+let rev = []
+
+for(let i = arr.length - 1; i >= 0; i--){
+    rev.push(arr[i])
+}
+
+console.log(rev)
