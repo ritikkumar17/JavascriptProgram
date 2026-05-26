17ef1d2cc5b5e53ba47153154ce429be9efcf735
ritik kumar <ritikchugh17@gmail.com>
2026-05-26 21:30:20 +0530

    find interscation of array

---
+++ array_programs/16_array.js
@@ -0,0 +1,30 @@
+// 16 find interscation of array
+
+let arr1 = [1,2,3,4]
+
+let arr2 = [3,4,5,6]
+
+let result = arr1.filter((num)=> arr2.includes(num))
+
+console.log(result)
+
+
+// 2nd way using loop
+
+// let arr1 = [1,2,3,4]
+
+// let arr2 = [3,4,5,6]
+
+// let result = []
+
+// for(let i = 0; i < arr1.length; i++){
+
+//     if(arr2.includes(arr1[i])){
+
+//         result.push(arr1[i])
+
+//     }
+
+// }
+
+// console.log(result)
