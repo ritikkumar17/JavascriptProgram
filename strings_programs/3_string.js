e49fe30f69f101e9d88f9ac29fe10ccc29d91c2b
ritik kumar <ritikchugh17@gmail.com>
2026-05-26 11:54:34 +0530

    3 Find Most Repeated Character and count

--- strings_programs/3_string.js
+++ strings_programs/3_string.js
@@ -1,40 +1,16 @@
-// 1 count Vowels
+// Find Most Reapeated Character
 
-// let input = "hellO world"
-// let data = input.toLowerCase()
-// let count=0
-
-// for(let i = 0; i< data.length; i++ ){
-//     let ch = data[i]
-//     if(ch =="a" || ch=="e"|| ch =="i" || ch=="o" || ch=="u"){
-//         count++
-//     }
-// }
-// console.log(count)
-
-// 2nd way
-
-let input = "ritik kumar"
-let data = input.toLowerCase()
-let count =0
-for(let i = 0; i< data.length; i++){
-    let ch="aeiou"
-    if(ch.includes(data[i])){
+let input = "javascripttp"
+let str =""
+let repeated=""
+let count=0
+for (let ch of input){
+      if(!str.includes(ch)){
+        str += ch
+      }else{
+        repeated += ch
         count++
-    }
+      }
 }
-console.log(count)
-
-
-// 3rd way
-let input = "ritik kumar"
-let count = 0
-let vowel =""
-for(let ch of input.toLowerCase()){
-    if("aeiou".includes(ch)){
-        vowel += ch
-        count++
-    }
-}
-console.log(count)
-console.log(vowel)
+console.log(str)
+console.log(repeated + " is repeated element " + count)
