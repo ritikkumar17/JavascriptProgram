8a9ec3d89eb9305238c64130491c84558bca9c80
ritik kumar <ritikchugh17@gmail.com>
2026-05-26 19:46:11 +0530

    15 Find All Pairs With Given SUM

---
+++ array_programs/15_array.js
@@ -0,0 +1,19 @@
+// Find All Pairs With Given Sum
+
+let arr = [1,2,3,4,5]
+
+let target = 5
+
+for(let i = 0; i < arr.length; i++){
+
+    for(let j = i + 1; j < arr.length; j++){
+
+        if(arr[i] + arr[j] === target){
+
+            console.log(arr[i], arr[j])
+
+        }
+
+    }
+
+}
