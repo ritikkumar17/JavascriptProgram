061144883497bf0a94fa8041d71c5a1be45435f2
ritik kumar <ritikchugh17@gmail.com>
2026-05-27 09:55:18 +0530

    10 Dynamic object Keys

---
+++ object_programs/10_object.js
@@ -0,0 +1,13 @@
+// 10 Dynamic object Keys
+
+const field1 = "name"
+const field2 = "age"
+const field3 = "city"
+
+const user = {
+    [field1]: "ritik",
+    [field2]: 24,
+    [field3]: "Punjab"
+}
+
+console.log(user)
