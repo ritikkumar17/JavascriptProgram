c78beefd4705e3b3ee09930f2d2248fe59e43b98
ritik kumar <ritikchugh17@gmail.com>
2026-05-27 08:21:13 +0530

    1 Create a object and access values , and update , add , delete

---
+++ object_programs/1_object.js
@@ -0,0 +1,27 @@
+// create and Access Object
+
+// let user ={
+//     name:"ritik",
+//     age:24,
+//     role:"Developer"
+// }
+
+// console.log(user)
+// console.log(user.role)
+// console.log(user["age"])
+
+
+
+// 2nd Do upadate , add , delete
+
+let user ={
+    name:"ritik",
+    age:24,
+    role:"Developer"
+}
+
+user.hobby ="codding"
+user.name="money"
+delete user.age
+
+console.log(user)
